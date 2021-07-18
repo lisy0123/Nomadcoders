4b5c1120d2b13b2a5633b100e9c377a38f41e150
lisy0123 <leesese3320@gmail.com>
2021-07-18 21:16:34 +0900

    ts update

---
+++ typescript/src/index.ts
@@ -0,0 +1,19 @@
+interface Human {
+    name: string;
+    age: number;
+    gender: string;
+}
+
+const person = {
+    name: "lisy",
+    age: 22,
+    gender: "female"
+};
+
+const sayHi = (person: Human): string => {
+    return (`Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`);
+};
+
+console.log(sayHi(person));
+
+export {};
